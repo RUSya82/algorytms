cd22bb852a8b896f1bea656a55947c86341b1718
RuslanSalimov <mail-spam2012@yandex.ru>
2024-02-25 15:11:56 +0500

    feat: add custom arrayMap

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-// Учитывая целочисленный массив arr и функцию фильтрации fn, верните отфильтрованный массив filteredArr.
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
 //
-//     Функция fn принимает один или два аргумента:
+//     The returned array should be created such that returnedArray[i] = fn(arr[i], i).
 //
-//     arr[i] - number from the arr
-// i - index of arr[i]
-// filteredArr должен содержать только элементы из arr, для которых вычисляется выражение fn(arr[i], i)
-// к истинному значению. Истинное значение — это значение, при котором Boolean(value) возвращает true.
-//
-//     Please solve it without the built-in Array.filter method.
+//     Please solve it without the built-in Array.map method.
 
 /**
  * @param {number[]} arr
  * @param {Function} fn
  * @return {number[]}
  */
-let filter = function(arr, fn) {
+let map = function(arr, fn) {
     const res = [];
     for (let i = 0; i < arr.length; i++) {
-        if(fn(arr[i], i)){
-            res.push(arr[i])
-        }
+        res[i] = fn(arr[i], i)
     }
-    return res
+    return res;
 };
-fn = function greaterThan10(n) { return n > 10; }
-const a = [0,10,20,30];
-console.log(filter(a,fn))
